65d85b2e68d9adfd84c3f9014dfea59ad0126f5a
jduncalf01 <jduncalf.01@gmail.com>
2020-12-10 20:07:53 -0800

    removed target=blank from GitHub link

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ export default function App() {
         <Weather />
         <footer>
          Coded by Jenna Duncalf and {" "}
-        <a href="https://github.com/jduncalf01/react-weather-app" target="_blank">open-sourced on GitHub</a>
+        <a href="https://github.com/jduncalf01/react-weather-app">open-sourced on GitHub</a>
       </footer>
     </div>
   </div>
