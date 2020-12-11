dc5898b2690d0d28b90d7b2c13ca4a32b59bbf8e
jduncalf01 <jduncalf.01@gmail.com>
2020-12-10 19:19:57 -0800

    Added GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -5,7 +5,11 @@ export default function App() {
   return (
     <div className="App">
       Weather App
-    </div>
+      <footer>
+        Coded by Jenna Duncalf and {" "}
+      <a href="https://github.com/jduncalf01/react-weather-app" target="_blank">open-sourced on GitHub</a>
+    </footer>
+  </div>
   );
 }
 
